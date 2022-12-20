4a5a0a04b54cdf06a1db92cfcb0618a70b84cee1
Alexey Isaev <montilarard@gmail.com>
2022-12-20 20:55:08 +0300

    Initial Commit

---
+++ AnimationProject3.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationProject3.01
+//
+//  Created by Алексей Исаев on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
